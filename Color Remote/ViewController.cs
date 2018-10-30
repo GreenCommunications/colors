fa69ebba19cb3371d7f85f567527ffbffa44094b
GreenCommunications <alexander.m.rankin@gmail.com>
2018-10-30 15:26:44 -0400

    init

---
+++ Color Remote/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+using CoreAnimation;
+
+namespace Color_Remote
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            View.BackgroundColor = UIColor.FromRGB(164, 164, 164);
+
+            contentView.Frame = new CGRect(0, View.SafeAreaInsets.Top + 70, UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height - 50);
+            var maskingShapeLayer = new CAShapeLayer()
+            {
+                Path = UIBezierPath.FromRoundedRect(contentView.Bounds, UIRectCorner.TopRight | UIRectCorner.TopLeft, new CGSize(25, 25)).CGPath
+            };
+            contentView.Layer.Mask = maskingShapeLayer;
+
+            headingLabel.Text = "Colors";
+            headingLabel.Font = UIFont.FromName("Roboto", 24);
+            headingLabel.SizeToFit();
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Color Remote/ViewController.designer.cs
@@ -0,0 +1,65 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Color_Remote
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider bS { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView contentView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider gS { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel headingLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider rS { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (bS != null) {
+                bS.Dispose ();
+                bS = null;
+            }
+
+            if (contentView != null) {
+                contentView.Dispose ();
+                contentView = null;
+            }
+
+            if (gS != null) {
+                gS.Dispose ();
+                gS = null;
+            }
+
+            if (headingLabel != null) {
+                headingLabel.Dispose ();
+                headingLabel = null;
+            }
+
+            if (rS != null) {
+                rS.Dispose ();
+                rS = null;
+            }
+        }
+    }
+}
